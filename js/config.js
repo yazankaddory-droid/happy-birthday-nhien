690f0192201d877e042dd74cda0764543dc28335
yazankaddory-droid <yazan.kaddory2@gmail.com>
2026-09-16 12:44:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,15 +14,7 @@ const SITE_CONFIG = {
   videoFile: "",
   placeholderMessage: `due to a problem on my side, i couldn't add a video :( but hopefully next time i can :)`,
   boxLabel: "Birthday of 2026 To Nhien",
-  message: `Haiiii. Happy birthday Nhien! welcome to being 17 years old! You are an amazing friend and i wish the best for you no matter what.
-
-I know we haven't been talking much recently but i want you to know that i still love and care for you. You are still one of my favorite people. Even though I've never truly vented to you in the past, but thank you for always being there for me.
-
-You're super nice, beautiful, gold hearted, loving, caring. I don't really know how i'd describe someone as amazing as you to someone that's never met you because there are no words to express how amazing you truly are.
-
-You never deserved any of the bad things that have happened to you and every day i wish that they never happened. I really hope your future is filled with nothing but happiness. You don't deserve anything bad because you're so kind and is actually honest. You respect yourself and is honest which makes you like, a super good person.
-
-I wish i could give you the biggest and most heart felt hug ever but sadly i can't sooooo, just know i want to then haha.
-
-Welcome to being 17 years old. Happy birthday queen! Just had to add a little gayness there. (this is just to make the line on top of this one visible on mobile). (okay so, it cut off again for me but if you see this and it isn't cut off. just know i added it BC it cut off)`
+  message: `HAPPY BIRTHDAYYYYYY NHIENNNNN, I would've left you a gift but can't bc of school and all 😅 so all I can do is write you a paragraph or smt. When we first met and started talking—after you told me "you're so tall, like giraffe", I never expected you to mean as much as you currently do. Playing with you, calling with you and texting with you became something I look forward to in my day. You mean so much to me and I care so much about you! 
+You're such an amazing and gold hearted person. You're going through stuff that you really don't deserve and I hope your life become easier and happier. If there is anything you ever need help with, im always here to try to help. 
+Welcome to being 17 years old!!!`
 };
